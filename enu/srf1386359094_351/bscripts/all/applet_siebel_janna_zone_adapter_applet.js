9eb25e52ec94bad83994ae795630cfe4e8e023dd
Raul Padilla <raul.padilla>
2013-12-11 16:39:03 +0000

    Siebel Release 8.1.3

---
+++ enu/srf1386359094_351/bscripts/all/applet_siebel_janna_zone_adapter_applet.js
@@ -0,0 +1,15 @@
+function Siebel_Janna_Zone_Adapter_Applet_Applet (applet)
+{
+   if (applet != null)
+   {
+      this._applet = applet;
+      applet.shadow = this;
+   }
+}
+
+new Siebel_Janna_Zone_Adapter_Applet_Applet (null);
+
+Siebel_Janna_Zone_Adapter_Applet_Applet.prototype = new top.JSSAppletShadow ();
+
+theApplication = top.theApplication;
+TheApplication = top.theApplication;
